6117b3a716d7e3b11139090dcd65db02b2961ffc
Dorota K <130973186+doriska1@users.noreply.github.com>
2023-04-28 09:58:07 +0200

    Add phone_number to Friend table

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,12 +2,13 @@
 #
 # Table name: friends
 #
-#  id         :bigint           not null, primary key
-#  email      :string
-#  name       :string
-#  surname    :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :bigint           not null, primary key
+#  email        :string
+#  name         :string
+#  phone_number :string
+#  surname      :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 class Friend < ApplicationRecord
 end
---
+++ db/migrate/20230428075348_add_phone_number_to_friend.rb
@@ -0,0 +1,5 @@
+class AddPhoneNumberToFriend < ActiveRecord::Migration[7.0]
+  def change
+    add_column :friends, :phone_number, :string
+  end
+end
